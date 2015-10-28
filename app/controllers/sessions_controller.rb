9073860959508adf0cfd162dc66cf62ea56e182d
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-28 18:18:53 +0000

    Add links to log in, log out, sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,20 @@ class SessionsController < ApplicationController
   end
 
 
-def create
-  user = User.find_by(email: params[:email])
+  def create
+    user = User.find_by(email: params[:email])
 
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
-  else
-    flash.now[:alert] = "Log in failed..."
-    render :new
-  end
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
+    else
+      flash.now[:alert] = "Log in failed..."
+      render :new
+    end
+
+    def destroy
+      session[:user_id] = nil
+      redirect_to movies_path, notice: "Adios!"
+    end 
+    
 end
