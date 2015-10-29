f56132d26187992cef45aecbf2d5db9a7fdac88f
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-29 19:35:29 +0000

    Forward root to movies/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 RottenMangoes::Application.routes.draw do
 
-  get "reviews/new"
-  get "reviews/create"
+
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
