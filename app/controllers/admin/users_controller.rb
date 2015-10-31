820301a5242129b06a9ba79264e932fda013640a
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-31 01:45:57 +0000

    Restrict access to admin page add link to admin page in the footer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,21 @@
+
 class Admin::UsersController < ApplicationController
   def index
+    puts '*' * 50
+    puts 'INDEX!'
+
+    @user = current_user
+    # binding.pry
+    
+    restrict_admin_acess
+    # restrict_admin_access
     @users = User.all
   end
 
   def restrict_admin_acess
-  if !user.admin?
-    flash[:alert] = "You are not an admin, fool"
-    redirect_to movies_path
-  end
+    if @user.admin == false
+      flash[:alert] = "You are not an admin, fool"
+      redirect_to movies_path
+    end
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-
+  
   def show
     @movie = Movie.find(params[:id])
   end
